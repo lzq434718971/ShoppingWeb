ac25e25a6c45a6e6d8dbfae21d81cc4e8f601227
lzq434718971 <434718971@qq.com>
2021-11-03 15:40:17 +0800

    newbuild

--- src/main/java/com/example/demo/controller/HWController.java
+++ src/main/java/com/example/demo/controller/HWController.java
@@ -1,14 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HWController {
-
+	
+	
     @RequestMapping("/hello")
     public String test() {
-        return "hot fix2";  
+        return "logintest";  
+    }
+    
+    
+    @PostMapping("/login")
+    public String login() {
+        return "logintest";
     }
 
 }
