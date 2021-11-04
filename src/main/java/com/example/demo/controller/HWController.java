7fa0b81f2cccb6a3fd6f20216b985d84ecf35bd8
lzq434718971 <434718971@qq.com>
2021-11-04 12:59:30 +0800

    newbuild

--- src/main/java/com/example/demo/controller/HWController.java
+++ src/main/java/com/example/demo/controller/HWController.java
@@ -1,24 +1,44 @@
 package com.example.demo.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HWController {
 	
 	
     @RequestMapping("/hello")
-    public String test() {
-        return "logintest";  
+    public String test(HttpServletRequest request,ModelMap model) {
+    	Principal principal=request.getUserPrincipal();
+    	if(principal != null)
+    	{
+    		log.info(principal.getName());
+        	model.addAttribute("userName",request.getUserPrincipal().getName());
+    	}
+    	else
+    	{
+    		model.addAttribute("userName",null);
+    	}
+        return "testindex";
     }
     
     
-    @PostMapping("/login")
-    public String login() {
-        return "logintest";
+    @GetMapping("/login")
+    public String login(HttpServletRequest request) {
+        return "loginPage";
     }
+    
 
 }
